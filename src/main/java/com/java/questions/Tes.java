dd4e1f249b12f0f8fddc201989f1f1b1e1883958
GaganGupta <gagan_gupta111@yahoo.com>
2021-03-24 17:57:02 +0530

    other

--- src/main/java/com/java/questions/Tes.java
+++ src/main/java/com/java/questions/Tes.java
@@ -4,6 +4,13 @@ public class Tes {
 
     public static void main(String[] args){
 
+
+        Object o = 10.0;
+
+        if (o instanceof Double){
+            System.out.println("True");
+        }
+
         System.out.println(5/2);
         System.out.println(5/2.0);
 
