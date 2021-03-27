f6bb926522d957c446b843b5dacfd577710da487
GaganGupta <gagan_gupta111@yahoo.com>
2021-03-27 18:00:14 +0530

    more

---
+++ src/main/java/com/coderpad/answers/low/DequeExample.java
@@ -0,0 +1,22 @@
+package com.coderpad.answers.low;
+
+public class DequeExample {
+
+    public static void main(String[] args){
+
+
+
+
+
+        }
+
+
+}
+
+
+
+interface DequeInterface{
+
+
+
+}
---
+++ src/main/java/com/coderpad/answers/low/lengthEncoding.java
@@ -0,0 +1,4 @@
+package com.coderpad.answers.low;
+
+public class lengthEncoding {
+}
